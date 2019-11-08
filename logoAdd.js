47591c45dd8fcf991f3d8a96716651356d5c22a5
zhaoyidi <zhaoyidi@bytedance.com>
2019-11-08 16:54:57 +0800

    支持日志输出

--- logoAdd.js
+++ logoAdd.js
@@ -1,4 +1,4 @@
-var childProcess = require('child_process');
+const { spawn } = require('child_process');
 var path = require('path');
 const {videoName} = require('./Config');
 const filePath = path.join('./VideoOverLay', videoName);
@@ -6,13 +6,16 @@ const filePath2 = path.join('./VideoAddLogo', videoName);
 const addTopLogo = `ffmpeg -y -i ${filePath} -vf "movie='./logo.png' [watermark];[in][watermark] overlay=21:7 [out]" ${filePath2}`;
 const addBottomLogo = `ffmpeg -y -i ${filePath} -vf "movie='./logo.png' [watermark];[in][watermark]  overlay=21:563 [out]" ${filePath2}`;
 
-const command = addTopLogo;
-console.log(command);
-childProcess.execSync(command, {maxBuffer: 5000 * 1024}, (error, stdout, stderr) => {
-    if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
+const args = addTopLogo.split(' ');
+args.shift();
+const ffmpeg = spawn('ffmpeg', args);
+
+ffmpeg.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+});
+ffmpeg.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+});
+ffmpeg.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
 });
--- overLay.js
+++ overLay.js
@@ -1,4 +1,4 @@
-var childProcess = require('child_process');
+const { spawn } = require('child_process');
 var path = require('path');
 const {videoName} = require('./Config');
 const filePath = path.join('./VideoOrigin', videoName);
@@ -6,13 +6,16 @@ const filePath2 = path.join('./VideoOverLay', videoName);
 const clearTopLogo = `ffmpeg -i ${filePath} -b:v 548k -vf delogo=x=21:y=7:w=132:h=152 ${filePath2}`;
 const clearBottomLogo = ` ffmpeg -i ${filePath} -b:v 548k -vf delogo=x=21:y=563:w=132:h=142 ${filePath2}`;
 
-const command = clearTopLogo;
-console.log(command);
-childProcess.execSync(command, {maxBuffer: 5000 * 1024}, (error, stdout, stderr) => {
-    if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
+const args = clearTopLogo.split(' ');
+args.shift();
+const ffmpeg = spawn('ffmpeg', args);
+
+ffmpeg.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+});
+ffmpeg.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+});
+ffmpeg.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
 });
